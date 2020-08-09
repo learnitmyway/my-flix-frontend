b6d525e463bb69781021b58668151c2c0da9b590
David <learnitmyway@users.noreply.github.com>
2020-08-09 17:01:53 +0200

    replace temp function with video service

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
-
-function temp() {
-  return [
-    {
-      id: '1',
-      src: 'assets/rock-balance.mp4',
-    },
-    {
-      id: '2',
-      src: 'assets/mare.mp4',
-    },
-    {
-      id: '3',
-      src: 'assets/waterfall.mp4',
-    },
-  ]
-}
+import { getVideos } from './video.service'
 
 function Video({ src }) {
   return (
@@ -35,14 +19,14 @@ function App() {
   const [videos, setVideos] = useState([])
   const [status, setStatus] = useState(statuses.LOADING)
 
-  async function getVideos() {
-    const videos = await temp()
+  async function fetchVideos() {
+    const videos = await getVideos()
     setVideos(videos)
     setStatus(statuses.LOADED)
   }
 
   useEffect(() => {
-    getVideos()
+    fetchVideos()
   }, [])
 
   return (
---
+++ src/video.service.js
@@ -0,0 +1,18 @@
+function getVideos() {
+  return [
+    {
+      id: '1',
+      src: 'assets/rock-balance.mp4',
+    },
+    {
+      id: '2',
+      src: 'assets/mare.mp4',
+    },
+    {
+      id: '3',
+      src: 'assets/waterfall.mp4',
+    },
+  ]
+}
+
+export { getVideos }
