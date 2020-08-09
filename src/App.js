f47aa386fc1b7a96bd66cb1c9fc236270090b4ef
David <learnitmyway@users.noreply.github.com>
2020-08-09 16:32:39 +0200

    extract video component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import './App.css'
 
+function Video({ src }) {
+  return (
+    <video width="750" controls>
+      <source src={src} type="video/mp4" />
+    </video>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <video width="750" controls>
-        <source src="assets/rock-balance.mp4" type="video/mp4" />
-      </video>
-      <video width="750" controls>
-        <source src="assets/mare.mp4" type="video/mp4" />
-      </video>
-      <video width="750" controls>
-        <source src="assets/waterfall.mp4" type="video/mp4" />
-      </video>
-    </div>
+    <main className="App">
+      <Video src="assets/rock-balance.mp4" />
+      <Video src="assets/mare.mp4" />
+      <Video src="assets/waterfall.mp4" />
+    </main>
   )
 }
 
